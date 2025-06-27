9666318addfa4234f260b4855ac17529bda15879
Maland <maland83@gmail.com>
2025-06-28 00:00:43 +0300

    Task 1 complited

---
+++ lib/main.dart
@@ -0,0 +1,61 @@
+import 'dart:math';
+
+void main(List<String> args) {
+  runTask1();
+  runTask2();
+  runTask3();
+}
+
+void runTask1() {
+  printSeperator("Task 1");
+
+  List<int> numbers = List.generate(100, (el) => Random().nextInt(101));
+  print(numbers);
+  print("\n65-й елемент: ${numbers[64]}");
+
+  // Вставте число 1000000000 на 50-ту позицію списку.
+  numbers.insert(51, 1000000000);
+
+  // Видаліть зі списку елементи зі значеннями: 24, 45, 66, 88.
+
+  List<int> numbersForRemove =
+      "24, 45, 66, 88".split(",").map<int>((e) {
+        return int.tryParse(e) ?? 0;
+      }).toList();
+
+  print("\nBefore removing: ${numbers.length}");
+  numbers.removeWhere((item) => numbersForRemove.contains(item));
+  print("After removing: ${numbers.length} \n");
+
+  int totalSum = 0;
+
+  for (var i = 0; i < numbers.length; i++) {
+    totalSum += numbers[i] % 3 == 0 ? numbers[i] : 0;
+  }
+  print(
+    "Сума всіх елементів, що діляться на 3 без залишку.: " +
+        totalSum.toString(),
+  );
+
+  List temp = [];
+  for (var element in numbers) {
+    if (element % 2 == 0) {
+      temp.add(element);
+    }
+  }
+  //  лише ті елементи, що діляться на 2 без залишку.
+  print("Довжина списку temp: ${temp.length}");
+}
+
+void runTask2() {
+  printSeperator("Task 2");
+}
+
+void runTask3() {
+  printSeperator("Task 3");
+}
+
+void printSeperator(String task) {
+  String part = List.filled(20, "-").join("");
+  print("${part} ${task} ${part}");
+}
